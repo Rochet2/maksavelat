38ee08248023a0ca2082b671c32c19abb15b3f5d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-06 10:51:41 +0000

    Deploy Rochet2/maksavelat to github.com/Rochet2/maksavelat.git:gh-pages

---
+++ precache-manifest.63e57e18498c316508e16e7d0e44b71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87bbe7c4dcf622e234b",
+    "url": "/maksavelat/static/css/main.8aa2a36a.chunk.css"
+  },
+  {
+    "revision": "c87bbe7c4dcf622e234b",
+    "url": "/maksavelat/static/js/main.c87bbe7c.chunk.js"
+  },
+  {
+    "revision": "3be05eb96759e1743f0f",
+    "url": "/maksavelat/static/js/1.3be05eb9.chunk.js"
+  },
+  {
+    "revision": "3164e04fb2b1891631dd",
+    "url": "/maksavelat/static/js/runtime~main.3164e04f.js"
+  },
+  {
+    "revision": "38ff8f7defa0f9bfc48bce6e18395893",
+    "url": "/maksavelat/index.html"
+  }
+];
