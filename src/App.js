f0b4c5bab5584126769dd24de37b3bdc80ad558c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-06 11:01:27 +0000

    Deploy Rochet2/maksavelat to github.com/Rochet2/maksavelat.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import MainPage from './pages/MainPage'
+import AddDebt from './pages/AddDebt'
+import Reasons from './pages/Reasons'
+import Debts from './pages/Debts'
+import Contacts from './pages/Contacts'
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename={'/maksavelat'}>
+        <div>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/AddDebt" component={AddDebt} />
+          <Route exact path="/Debts" component={Debts} />
+          <Route exact path="/Reasons" component={Reasons} />
+          <Route exact path="/Contacts" component={Contacts} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Reasons.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { array, func, bool } from 'prop-types'
+import { Header, Button, Segment, Input, List } from 'semantic-ui-react'
+
+class Reasons extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+    }
+
+    render() {
+        const { whiteTheme, dispatch, reasons } = this.props
+        return (
+            <div>
+                <Segment inverted={whiteTheme} basic style={{ height: '100vh' }}>
+                    <Header size="large">Reasons</Header>
+                    <Input
+                        action={{ content: 'Add reason', onClick: () => {
+                            dispatch({ type: 'AddReason', value: this.state.value })
+                            this.setState({ value: '' })
+                        }}}
+                        placeholder='Reason name...'
+                        onChange={(e => this.setState({ value: e.target.value }))}
+                        value={this.state.value}
+                    />
+                    <List inverted={whiteTheme} divided>
+                        {reasons.map(reason => (
+                            <List.Item key={reason}>
+                                <List.Content floated="right">
+                                    <Button
+                                        color='red'
+                                        content={'Remove'}
+                                        icon='remove'
+                                        onClick={() => {
+                                            dispatch({ type: 'RemoveReason', value: reason })
+                                        }}
+                                    />
+                                </List.Content>
+                                <List.Header>
+                                    {reason}
+                                </List.Header>
+                            </List.Item>
+                        ))}
+                    </List>
+                </Segment>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        reasons: state.reasons,
+        whiteTheme: state.whiteTheme,
+    }
+}
+
+Reasons.propTypes = {
+    reasons: array.isRequired,
+    dispatch: func.isRequired,
+    whiteTheme: bool.isRequired,
+}
+
+export default connect(mapStateToProps)(Reasons)
